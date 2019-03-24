e9fea9bdcb5446745137b76cb51576e18c349f2e
Sahil Sinh <sahilsingh.dsc@gmail.com>
2019-03-24 19:46:23 +0530

    initial commit for solc

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.17;
+
+contract Inbox {
+    string public message;
+    
+    constructor (string memory initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+    
+}
